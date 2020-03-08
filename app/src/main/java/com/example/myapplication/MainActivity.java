7b80e6c92665ed6a9996bfda31dac459b90fe50a
Mculover666 <2412828003@qq.com>
2020-03-08 10:34:14 +0800

    界面设计完成

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,33 +4,149 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    private MqttClient client;
+    private MqttConnectOptions options;
+
+    private String host = "tcp://122.51.89.94:1883";
+    private String userName = "android";
+    private String passWord = "android";
+    private String mqtt_id = "client-0002";
+
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button login_button = (Button)findViewById(R.id.button_exit);
+//        Button login_button = (Button)findViewById(R.id.button_exit);
+//
+//        login_button.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//
+//                Toast.makeText(MainActivity.this, "成功",Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+    }
 
-        login_button.setOnClickListener(new View.OnClickListener(){
+
+    //Mqtt初始化函数
+    private void Mqtt_init()
+    {
+        try {
+            //host为主机名，test为clientid即连接MQTT的客户端ID，一般以客户端唯一标识符表示，MemoryPersistence设置clientid的保存形式，默认为以内存保存
+            client = new MqttClient(host, mqtt_id,
+                    new MemoryPersistence());
+            //MQTT的连接设置
+            options = new MqttConnectOptions();
+            //设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接
+            options.setCleanSession(false);
+            //设置连接的用户名
+            options.setUserName(userName);
+            //设置连接的密码
+            options.setPassword(passWord.toCharArray());
+            // 设置超时时间 单位为秒
+            options.setConnectionTimeout(10);
+            // 设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送个消息判断客户端是否在线，但这个方法并没有重连的机制
+            options.setKeepAliveInterval(20);
+            //设置回调
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    //连接丢失后，一般在这里面进行重连
+                    System.out.println("connectionLost----------");
+                    //startReconnect();
+                }
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    //publish后会执行到这里
+                    System.out.println("deliveryComplete---------"
+                            + token.isComplete());
+                }
+                @Override
+                public void messageArrived(String topicName, MqttMessage message)
+                        throws Exception {
+                    //subscribe后得到的消息会执行到这里面
+                    System.out.println("messageArrived----------");
+                    Message msg = new Message();
+                    msg.what = 3;   //收到消息标志位
+                    msg.obj = topicName + "---" + message.toString();
+                    handler.sendMessage(msg);    // hander 回传
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //MQTT连接EMQ-X函数
+    private void Mqtt_connect() {
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "退出成功",Toast.LENGTH_SHORT).show();
-
-                //构造一个Intent
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                //启动下一个活动
-                startActivity(intent);
+            public void run() {
+                try {
+                    if(!(client.isConnected()) )  //如果还未连接
+                    {
+                        client.connect(options);
+                        Message msg = new Message();
+                        msg.what = 31;
+                        handler.sendMessage(msg);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Message msg = new Message();
+                    msg.what = 30;
+                    handler.sendMessage(msg);
+                }
             }
-        });
+        }).start();
+    }
+    //MQTT重连
+    private void startReconnect() {
+//        scheduler = Executors.newSingleThreadScheduledExecutor();
+//        scheduler.scheduleAtFixedRate(new Runnable() {
+//            @Override
+//            public void run() {
+//                if (!client.isConnected()) {
+//                    Mqtt_connect();
+//                }
+//            }
+//        }, 0 * 1000, 10 * 1000, TimeUnit.MILLISECONDS);
+    }
+    //MQTT发布消息
+    private void publishmessageplus(String topic,String message2)
+    {
+        if (client == null || !client.isConnected()) {
+            return;
+        }
+        MqttMessage message = new MqttMessage();
+        message.setPayload(message2.getBytes());
+        try {
+            client.publish(topic,message);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
