1f73c4fc398769f4b01970fd00851f9d7dab9f63
mculover666 <2412828003@qq.com>
2020-09-18 16:59:12 +0800

    加入登录界面

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -6,7 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import android.util.Log;
+
+import org.w3c.dom.Text;
+
+import static android.util.Log.i;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +27,20 @@ public class LoginActivity extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                TextView username_textview = (TextView)findViewById(R.id.username_edittext);
+                TextView passwd_textview = (TextView)findViewById(R.id.passwd_edittext);
+
+                String username = username_textview.getText().toString();
+                String passwd = passwd_textview.getText().toString();
+
+                Log.i("LoginActivity", "usename:"+username);
+                Log.i("LoginActivity", "passwd:"+passwd);
+
+                if (username.equals("2016211961") != true || passwd.equals("123456") != true) {
+                    Toast.makeText(LoginActivity.this, "啊哦~登录失败！",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 Toast.makeText(LoginActivity.this, "登录成功",Toast.LENGTH_SHORT).show();
 
                 //构造一个Intent
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -116,13 +116,10 @@ public class MainActivity extends AppCompatActivity {
         min_gas_edittext = findViewById(R.id.min_gas_edittext);
 
         Button login_button = (Button)findViewById(R.id.button_timer);
-
         login_button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 showTimePickerDialog(MainActivity.this,  4, txtTime, calendar);
-                //Toast.makeText(MainActivity.this, "成功",Toast.LENGTH_SHORT).show();
-
             }
         });
 
@@ -201,7 +198,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //设置亮度最大按钮回调函数
+        //设置甲醛最大按钮回调函数
         max_light_set_btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -219,7 +216,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //设置亮度最小按钮回调函数
+        //设置甲醛最小按钮回调函数
         min_light_set_btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -278,25 +275,20 @@ public class MainActivity extends AppCompatActivity {
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
                 switch (msg.what){
-                    case 1: //开机校验更新回传
+                    case 1: 
                         break;
-                    case 2:  // 反馈回传
-
+                    case 2:
                         break;
-                    case 3:  //MQTT 收到消息回传   UTF8Buffer msg=new UTF8Buffer(object.toString());
-                        //Toast.makeText(MainActivity.this,msg.obj.toString() ,Toast.LENGTH_SHORT).show();
+                    case 3:
                         temp_text_view.setText(msg.obj.toString());
                         break;
-                    case 4:  //MQTT 收到消息回传   UTF8Buffer msg=new UTF8Buffer(object.toString());
-                        //Toast.makeText(MainActivity.this,msg.obj.toString() ,Toast.LENGTH_SHORT).show();
+                    case 4: 
                         humi_text_view.setText(msg.obj.toString());
                         break;
-                    case 5:  //MQTT 收到消息回传   UTF8Buffer msg=new UTF8Buffer(object.toString());
-                        //Toast.makeText(MainActivity.this,msg.obj.toString() ,Toast.LENGTH_SHORT).show();
+                    case 5:  
                         lightness_text_view.setText(msg.obj.toString());
                         break;
-                    case 6:  //MQTT 收到消息回传   UTF8Buffer msg=new UTF8Buffer(object.toString());
-                        //Toast.makeText(MainActivity.this,msg.obj.toString() ,Toast.LENGTH_SHORT).show();
+                    case 6:  
                         gas_text_view.setText(msg.obj.toString());
                         break;
                     case 30:  //连接失败
@@ -305,13 +297,9 @@ public class MainActivity extends AppCompatActivity {
                     case 31:   //连接成功
                         Toast.makeText(MainActivity.this,"连接成功" ,Toast.LENGTH_SHORT).show();
                         try {
-                            //订阅温度数据
                             client.subscribe("temp",1);
-                            //订阅湿度数据
                             client.subscribe("humi",1);
-                            //订阅亮度数据
-                            client.subscribe("lightness",1);
-                            //订阅气体数据
+                            client.subscribe("ch20",1);
                             client.subscribe("gas",1);
                         } catch (MqttException e) {
                             e.printStackTrace();
@@ -355,17 +343,7 @@ public class MainActivity extends AppCompatActivity {
         return "";
     }
 
-    /**
-     * 时间选择
-     * @param activity
-     * @param themeResId
-     * @param tv
-     * @param calendar
-     */
     public void showTimePickerDialog(Activity activity, int themeResId, final TextView tv, Calendar calendar) {
-        // Calendar c = Calendar.getInstance();
-        // 创建一个TimePickerDialog实例，并把它显示出来
-        // 解释一哈，Activity是context的子类
         new TimePickerDialog( activity,themeResId,
                 // 绑定监听器
                 new TimePickerDialog.OnTimeSetListener() {
@@ -380,8 +358,6 @@ public class MainActivity extends AppCompatActivity {
                         catch (Exception e) {
                             Toast.makeText(MainActivity.this, "定时失败",Toast.LENGTH_SHORT).show();
                         }
-
-
                     }
                 }
                 // 设置初始时间
@@ -395,39 +371,37 @@ public class MainActivity extends AppCompatActivity {
     private void Mqtt_init()
     {
         try {
-            //host为主机名，test为clientid即连接MQTT的客户端ID，一般以客户端唯一标识符表示，MemoryPersistence设置clientid的保存形式，默认为以内存保存
             client = new MqttClient(host, mqtt_id,
                     new MemoryPersistence());
-            //MQTT的连接设置
             options = new MqttConnectOptions();
-            //设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接
+           
             options.setCleanSession(false);
-            //设置连接的用户名
+            
             options.setUserName(userName);
-            //设置连接的密码
+            
             options.setPassword(passWord.toCharArray());
-            // 设置超时时间 单位为秒
+            
             options.setConnectionTimeout(10);
-            // 设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送个消息判断客户端是否在线，但这个方法并没有重连的机制
+            
             options.setKeepAliveInterval(20);
-            //设置回调
+            
             client.setCallback(new MqttCallback() {
                 @Override
                 public void connectionLost(Throwable cause) {
-                    //连接丢失后，一般在这里面进行重连
+                   
                     System.out.println("connectionLost----------");
-                    //startReconnect();
+                   
                 }
                 @Override
                 public void deliveryComplete(IMqttDeliveryToken token) {
-                    //publish后会执行到这里
+                   
                     System.out.println("deliveryComplete---------"
                             + token.isComplete());
                 }
                 @Override
                 public void messageArrived(String topicName, MqttMessage message)
                         throws Exception {
-                    //subscribe后得到的消息会执行到这里面
+                   
                     System.out.println("messageArrived----------");
                     Message msg = new Message();
                     if(topicName.equals("temp"))
@@ -444,18 +418,18 @@ public class MainActivity extends AppCompatActivity {
                         msg.obj = message.toString() + " %RPH";
                         handler.sendMessage(msg);    // hander 回传
                     }
-                    else if(topicName.equals("lightness"))
+                    else if(topicName.equals("ch20"))
                     {
                         //收到亮度数据
                         msg.what = 5;   //收到消息标志位
-                        msg.obj = message.toString() + " Lux";
+                        msg.obj = message.toString() + " mg/m3";
                         handler.sendMessage(msg);    // hander 回传
                     }
                     else if(topicName.equals("gas"))
                     {
                         //收到气体数据
                         msg.what = 6;   //收到消息标志位
-                        msg.obj = message.toString() + " %";
+                        msg.obj = message.toString() + " ug/m3";
                         handler.sendMessage(msg);    // hander 回传
                     }
                 }
